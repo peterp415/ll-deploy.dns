f38e195c053a6c7bcdb3549e95a0ab03205ee782
steven.bauer <steven.bauer@locationlabs.com>
2016-04-28 15:02:41 -0700

    Modified rsync script to save zones to /masters/ folder

--- migration_scripts/rsync_bind_zones.sh
+++ migration_scripts/rsync_bind_zones.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-server_list="control.evdc.wavemarket.com
-ns3.sfdc.wavemarket.com
-ns5.sfdc.wavemarket.com
-base1.engr.wavemarket.com"
+server_list_incl_zones="control.evdc.wavemarket.com
+ns3.wavemarket.com
+ns5.sfdc.wavemarket.com"
+
+server_list="base1.engr.wavemarket.com
+base2.engr.wavemarket.com"
+
+rm ../masters/*
+
+#get the 
+for current_server in $server_list_incl_zones; do
+    #mkdir ../masters/$current_server
+
+    #Get all files from both /etc/bind
+    zone_dir='/etc/bind/zones/'
+    rsync -a root@$current_server:$zone_dir ../masters/
+done
 
 for current_server in $server_list; do
-    mkdir $current_server
-
-    zone_dir="/etc/bind/"
-
-    if [ -d "/etc/bind/zones/" ]; then
-        zone_dir="/etc/bind/zones/"
-    fi
-    rsync -a root@$current_server:$zone_dir $current_server
+    #mkdir ../masters/$current_server
+    zone_dir='/etc/bind/'
+    rsync -a root@$current_server:$zone_dir ../masters/
 done
