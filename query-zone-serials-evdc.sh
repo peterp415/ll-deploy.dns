c235a2a96a5bf16debfd4b28b004959df32a8b2e
Peter Pletcher <peter.pletcher@locationlabs.com>
2016-05-09 14:24:27 -0700

    add some useful test scripts that query zone data from populations of servers

---
+++ query-zone-serials-evdc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Query dns servers for the SOA serial in each zone
+
+for dnszone in evdc.wavemarket.com 172.IN-ADDR.ARPA ;do
+  for dnssrv in ns1.evdc.wavemarket.com ns2.evdc.wavemarket.com ns1.engr.wavemarket.com ns2.engr.wavemarket.com ;do
+    echo -n "${dnssrv} ${dnszone} : " ; dig @${dnssrv} -t soa +short ${dnszone} 2>/dev/null | awk '{print $3}'
+  done
+done
