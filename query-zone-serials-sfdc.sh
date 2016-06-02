23983c0cc4ad934f87f8ddbce80a18904de4bd33
Peter Pletcher <peter.pletcher@locationlabs.com>
2016-06-02 13:21:18 -0700

    Added quick zone serial check for SFDC elder servers

---
+++ query-zone-serials-sfdc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Query dns servers for the SOA serial in each zone
+
+# SFDC
+for dnszone in sfdc.wavemarket.com ;do
+  for dnssrv in ns1.sfdc.wavemarket.com ns2.sfdc.wavemarket.com ;do
+    echo -n "${dnssrv} ${dnszone} : " ; ssh root@${dnssrv} "dig @localhost -t soa +short ${dnszone} 2>/dev/null" | awk '{print $3}'
+  done
+done
