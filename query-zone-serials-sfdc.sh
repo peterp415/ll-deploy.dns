7a0524a3a0eb01f42ec4cd3809a7a7a6fe02a154
Peter Pletcher <peter.pletcher@locationlabs.com>
2016-06-02 13:29:06 -0700

    have test script query common and silo servers for sfdc zone

--- query-zone-serials-sfdc.sh
+++ query-zone-serials-sfdc.sh
@@ -4,7 +4,7 @@
 
 # SFDC
 for dnszone in sfdc.wavemarket.com ;do
-  for dnssrv in ns1.sfdc.wavemarket.com ns2.sfdc.wavemarket.com ;do
+  for dnssrv in ns1.sfdc.wavemarket.com ns2.sfdc.wavemarket.com ns{1,2}.{common,aci,afm,sfp,tfw}.sfdc.wavemarket.com ;do
     echo -n "${dnssrv} ${dnszone} : " ; ssh root@${dnssrv} "dig @localhost -t soa +short ${dnszone} 2>/dev/null" | awk '{print $3}'
   done
 done
